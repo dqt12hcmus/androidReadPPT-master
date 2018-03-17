757978079fd0805d345c843720a72ddec35af131
dqt12hcmus <doquangthai12@gmail.com>
2018-03-17 13:43:26 +0700

    1st

--- src/com/poi/poiandroid/MainActivity.java
+++ src/com/poi/poiandroid/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		addControls();
+		addControls();//
 		addEvents();
 	}
 	private void addEvents() {
